b22a26216065a6c3d163779ea7df6c4bb141e323
Henry <henrybrs@gmail.com>
2016-01-29 17:18:41 -0200

    Create Home view

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    '''GET / must return status code 200'''
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        ''' Must use core/index.html'''
+        self.assertTemplateUsed(self.response, 'core/index.html')
