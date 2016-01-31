2e63118447c98fef804a02d473430bd1cb81056d
Henry <henrybrs@gmail.com>
2016-01-31 20:10:24 -0200

    Correct test syntax and make use of reverse to avoid absolute paths

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,12 +1,14 @@
+# coding: utf-8
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.response = self.client.get('/')
+        self.response = self.client.get(reverse('home'))
 
-    '''GET / must return status code 200'''
     def test_get(self):
+        '''GET / must return status code 200'''
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
