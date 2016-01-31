9d3f966d94e91a763e483c82f7dfe9b2469f4e10
Henry <henrybrs@gmail.com>
2016-01-31 20:22:49 -0200

    Override Zipcode API create method to get info from third party * using only the zipcode the api must be able to find address info * using third-party Postmon

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -14,3 +14,33 @@ class HomeTest(TestCase):
     def test_template(self):
         ''' Must use core/index.html'''
         self.assertTemplateUsed(self.response, 'core/index.html')
+
+
+class ZipCodeCreateTest(TestCase):
+    def test_create_zipcode_scenario_1(self):
+        response = self.client.post('/zipcode/', data={'zip_code': 14020260})
+        self.assertEqual(201, response.status_code)
+        self.assertTrue(response.data['state'])
+        self.assertTrue(response.data['city'])
+        self.assertTrue(response.data['neighborhood'])
+        self.assertTrue(response.data['address'])
+
+    def test_create_zipcode_scenario_2(self):
+        response = self.client.post('/zipcode/', data={'zip_code': 14110000})
+        self.assertEqual(201, response.status_code)
+        self.assertTrue(response.data['state'])
+        self.assertTrue(response.data['city'])
+        self.assertFalse(response.data['neighborhood'])
+        self.assertFalse(response.data['address'])
+
+    def test_create_zipcode_scenario_3(self):
+        response = self.client.post('/zipcode/', data={'zip_code': 20040972})
+        self.assertEqual(201, response.status_code)
+        self.assertTrue(response.data['state'])
+        self.assertTrue(response.data['city'])
+        self.assertTrue(response.data['neighborhood'])
+        self.assertFalse(response.data['address'])
+
+    def test_false_zipcode(self):
+        response = self.client.post('/zipcode/', data={'zip_code': 1402260})
+        self.assertEqual(404, response.status_code)
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,13 @@
 # coding: utf-8
 from __future__ import absolute_import
+import requests
+import json
+from django.http import Http404
 from django.views.generic import TemplateView
 from apps.core.models import Zipcode
 
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import viewsets
 from apps.core.serializers import ZipcodeSerializer
 
@@ -14,3 +19,28 @@ class Home(TemplateView):
 class ZipcodeViewSet(viewsets.ModelViewSet):
     queryset = Zipcode.objects.all()
     serializer_class = ZipcodeSerializer
+
+    def create(self, request, *args, **kwargs):
+        postmon = requests.get(
+            'http://api.postmon.com.br/v1/cep/%s' % (request.data['zip_code']))
+        if postmon.status_code == 200:
+            postmon_data = json.loads(postmon.content)
+            cleaned_data = self.clean_zipcode(postmon_data)
+        else:
+            raise Http404('Não foi possível encontrar dados do cep no Postmon')
+
+        serializer = self.get_serializer(data=cleaned_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def clean_zipcode(self, data):
+        cleaned_data = {}
+        for field in Zipcode._meta.fields:
+            if field.verbose_name.lower() in data:
+                cleaned_data[field.name] = data[field.verbose_name.lower()]
+            else:
+                cleaned_data[field.name] = ''
+        return cleaned_data
