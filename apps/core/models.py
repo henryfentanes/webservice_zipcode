eb062ee10ade7c8d04cac6417b8003e7cc6cf5fe
Henry <henrybrs@gmail.com>
2016-01-31 20:07:20 -0200

    Create model for Zipcode

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Zipcode(models.Model):
+    zip_code = models.CharField('CEP', max_length=8, primary_key=True)
+    address = models.CharField(
+        'Logradouro', max_length=100, blank=True, null=True)
+    neighborhood = models.CharField(
+        'Bairro', max_length=50, blank=True, null=True)
+    state = models.CharField('Estado', max_length=2, blank=True, null=True)
+    city = models.CharField('Cidade', max_length=50, blank=True, null=True)
